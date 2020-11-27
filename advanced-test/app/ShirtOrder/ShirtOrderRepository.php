9210dae30b9657c871860dfb4b76a89ed5f183bd
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-27 21:53:09 +0700

    [ ADD ] ShirtOrderRepository

---
+++ advanced-test/app/ShirtOrder/ShirtOrderRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\ShirtOrder;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+
+interface ShirtOrderRepository
+{
+
+}
