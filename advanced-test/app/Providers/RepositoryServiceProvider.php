7f416b3dd7cfd5bf3eb6d00a4bd615dbf48164e7
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-27 22:12:57 +0700

    [ ADD ] Repository Service Provider

---
+++ advanced-test/app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'App\ShirtOrder\ShirtOrderRepository',
+            'App\ShirtOrder\EloquentRepository'
+        );
+    }
+
+}
