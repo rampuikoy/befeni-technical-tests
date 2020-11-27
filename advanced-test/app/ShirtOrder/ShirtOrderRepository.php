12822daebeaa99768f3bdf88bb14c1137eefee31
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-27 21:57:01 +0700

    [ EDIT ] add function to ShirtOrderRepository

--- advanced-test/app/ShirtOrder/ShirtOrderRepository.php
+++ advanced-test/app/ShirtOrder/ShirtOrderRepository.php
@@ -7,5 +7,49 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface ShirtOrderRepository
 {
+    /**
+     * create new shirt
+     *
+     * @param array $data
+     *
+     * @return object|null
+     */
+    public function store(array $data);
 
+    /**
+     * update shirt by id
+     *
+     * @param $id
+     * @param array $data
+     *
+     * @return object
+     */
+    public function updateById($id, array $data);
+
+    /**
+     * get shirt by id
+     *
+     * @param $id
+     *
+     * @return object
+     */
+    public function getById($id);
+
+    /**
+     * delete shirt order by id
+     *
+     * @param $id
+     *
+     * @return object|null
+     */
+    public function DeleteById($id);
+
+     /**
+     * search shirt order
+     *
+     * @param $query
+     *
+     * @return Collection
+     */
+    public function search (string $query = ''): Collection;
 }
