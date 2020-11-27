242e56bebc78bdeb149eae5369b94816d42889e1
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-27 15:41:41 +0700

    [ ADD ] function reading file.

--- basic-test/index.php
+++ basic-test/index.php
@@ -4,8 +4,16 @@
 <body>
 
     <?php
-    $txt = "PHP";
-    echo "I love $txt!";
+        function prepare_data_from_file(&$num, &$opt){
+            $myfile = fopen("datafile.txt", "r") or die("Unable to open file!");
+            // Output one line until end-of-file
+            while (!feof($myfile)) {
+                echo fgets($myfile) . "<br>";
+            }
+            fclose($myfile);
+        }
+
+        prepare_data_from_file();
     ?>
 
 </body>
