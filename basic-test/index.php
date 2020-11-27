218656b902dbe3a3f0966252130c5b88abbc62fc
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-27 16:14:39 +0700

    [ ADD ] Spliting String function And keep it in array.

--- basic-test/index.php
+++ basic-test/index.php
@@ -4,16 +4,36 @@
 <body>
 
     <?php
-        function prepare_data_from_file(&$num, &$opt){
-            $myfile = fopen("datafile.txt", "r") or die("Unable to open file!");
-            // Output one line until end-of-file
-            while (!feof($myfile)) {
-                echo fgets($myfile) . "<br>";
-            }
-            fclose($myfile);
-        }
 
-        prepare_data_from_file();
+    $start = 0;
+    $number = [];
+    $operator = [];
+
+    function split_text($text)
+    {
+        return explode(" ", $text);
+    }
+
+    function prepare_data_from_file(&$num, &$opt, &$first)
+    {
+        $myfile = fopen("datafile.txt", "r") or die("Unable to open file!");
+
+        while (!feof($myfile)) {
+            $split_str = split_text((fgets($myfile)));
+            echo $split_str[0] . " ";
+            echo $split_str[1] . "<br>";
+            if ($split_str[0] == 'apply') {
+                $first = $split_str[1];
+            } else {
+                $num[] = $split_str[1];
+                $opt[] = $split_str[0];
+            }
+        }
+        fclose($myfile);
+    }
+
+    prepare_data_from_file($number, $operator, $start);
+
     ?>
 
 </body>
