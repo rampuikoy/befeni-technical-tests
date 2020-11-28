10fbb6c723d0690d63a3d2ec09e28af209512aea
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-28 12:52:04 +0700

    [ ADD ] function to Shirt Controller

--- advanced-test/app/Http/Controllers/ShirtOrderController.php
+++ advanced-test/app/Http/Controllers/ShirtOrderController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ShirtOrder\ShirtOrderInsertRequest;
+use App\Http\Requests\ShirtOrder\ShirtOrderUpdateRequest;
+use App\ShirtOrder\ShirtOrderRepository;
+use App\Traits\CoreResponseTrait;
 use Illuminate\Http\Request;
 
 class ShirtOrderController extends Controller
 {
-    //
+    use CoreResponseTrait;
+
+    protected $shirtOrderRepo;
+
+    public function __construct(ShirtOrderRepository $shirtOrderRepo)
+    {
+        $this->shirtOrderRepo = $shirtOrderRepo;
+    }
+
+    public function insert(ShirtOrderInsertRequest $request)
+    {
+        $data = $request->only(
+            'customer_id',
+            'fabric_id',
+            'collar_size',
+            'chest_size',
+            'waist_size',
+            'wrist_size',
+        );
+
+        $shirt = $this->shirtOrderRepo->store($data);
+
+        return $this->successResponse($shirt);
+    }
+
+    public function updateById(ShirtOrderUpdateRequest $request, $id)
+    {
+        $data = $request->only(
+            'customer_id',
+            'fabric_id',
+            'collar_size',
+            'chest_size',
+            'waist_size',
+            'wrist_size',
+        );
+
+        $shirt = $this->shirtOrderRepo->updateById($data, $id);
+
+        return $this->successResponse($shirt);
+    }
+
+    public function getById($id)
+    {
+        $shirt = $this->shirtOrderRepo->getById($id);
+
+        return $this->successResponse($shirt);
+    }
+
+    public function deleteById($id)
+    {
+        $shirt = $this->shirtOrderRepo->deleteById($id);
+
+        return $this->successResponse($shirt);
+    }
+
+    public function search()
+    {
+        //
+    }
 }
