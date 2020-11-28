27e7a3ac7fe50bb6a048242603a592705dd9f60f
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-28 12:53:06 +0700

    [ ADD ] Validation for update ShirtOrder model

---
+++ advanced-test/app/Http/Requests/ShirtOrder/ShirtOrderUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\ShirtOrder;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShirtOrderUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'customer_id' => 'integer',
+            'fabric_id' => 'integer',
+            'collar_size' => 'integer',
+            'chest_size' => 'integer',
+            'waist_size' => 'integer',
+            'wrist_size' => 'integer',
+        ];
+    }
+}
