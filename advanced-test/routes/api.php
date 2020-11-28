fb357f778461d070c3467d33dd74287cddd1bd62
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-28 14:37:46 +0700

    [ EDIT ] add Shirt order service API ( create , update , delete , get by id )

--- advanced-test/routes/api.php
+++ advanced-test/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ShirtOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'shirt-order'
+], function () {
+    Route::get('/{id}', [ShirtOrderController::class, 'getById']);
+    Route::post('/create', [ShirtOrderController::class, 'insert']);
+    Route::put('/{id}', [ShirtOrderController::class, 'updateById']);
+    Route::delete('/{id}', [ShirtOrderController::class, 'deleteById']);
 });
