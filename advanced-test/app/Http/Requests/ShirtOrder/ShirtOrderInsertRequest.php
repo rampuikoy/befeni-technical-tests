7d58f32b562adbe4be0c0b520a90bfd82f11aee2
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-28 18:57:50 +0700

    [ EDIT ] make validation authorize true

--- advanced-test/app/Http/Requests/ShirtOrder/ShirtOrderInsertRequest.php
+++ advanced-test/app/Http/Requests/ShirtOrder/ShirtOrderInsertRequest.php
@@ -13,7 +13,7 @@ class ShirtOrderInsertRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- advanced-test/app/Http/Requests/ShirtOrder/ShirtOrderUpdateRequest.php
+++ advanced-test/app/Http/Requests/ShirtOrder/ShirtOrderUpdateRequest.php
@@ -13,7 +13,7 @@ class ShirtOrderUpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
