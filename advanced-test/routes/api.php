e4b1de97a93933326ad86b2eb5097d042ca4a908
Pongsathorn Wongpayakyothin <phongsathon.tuey@gmail.com>
2020-11-30 13:26:11 +0700

    [ ADD ] create route for all controller

--- advanced-test/routes/api.php
+++ advanced-test/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DataSourceController;
 use App\Http\Controllers\ShirtOrderController;
+use App\Http\Controllers\TestResourceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,10 +23,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group([
     'middleware' => 'json.response',
+    'prefix' => 'test-resource'
+], function () {
+    Route::get('/', [TestResourceController::class, 'getById']);
+    Route::post('/create', [TestResourceController::class, 'insert']);
+    Route::put('/', [TestResourceController::class, 'updateById']);
+    Route::delete('/', [TestResourceController::class, 'deleteById']);
+    Route::post('/datasource', [ShirtOrderController::class, 'datasource']);
+});
+
+Route::group([
+    'middleware' => 'json.response',
+    'prefix' => 'data-source'
+], function () {
+    Route::get('/', [DataSourceController::class, 'all']);
+    Route::post('/', [DataSourceController::class, 'create']);
+    Route::get('/{tag}/{type}', [DataSourceController::class, 'get']);
+    Route::delete('/{tag}/{type}', [DataSourceController::class, 'delete']);
+});
+
+Route::group([
+    'middleware' => 'json.response',
     'prefix' => 'shirt-order'
 ], function () {
-    Route::get('/{id}', [ShirtOrderController::class, 'getById']);
-    Route::post('/create', [ShirtOrderController::class, 'insert']);
-    Route::put('/{id}', [ShirtOrderController::class, 'updateById']);
-    Route::delete('/{id}', [ShirtOrderController::class, 'deleteById']);
+    Route::post('/receive', [ShirtOrderController::class, 'receive']);
+    Route::post('/create', [ShirtOrderController::class, 'create']);
+    Route::post('/update', [ShirtOrderController::class, 'update']);
+    Route::post('/delete', [ShirtOrderController::class, 'delete']);
 });
